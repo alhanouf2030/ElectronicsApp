7ef11c5314d06cda7459f162cce2e07e4a28f8f0
alhanouf2030 <alhanouf228@gmail.com>
2022-04-04 10:09:07 +0300

    Initial Commit

---
+++ ElectronicsApp/ElectronicsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElectronicsAppApp.swift
+//  ElectronicsApp
+//
+//  Created by alhanouf on 03/09/1443 AH.
+//
+
+import SwiftUI
+
+@main
+struct ElectronicsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
